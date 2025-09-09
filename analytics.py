c7597f9d7623f305b05326d737ab16115e8504ae
Jakub Gania <contact@jakubgania.io>
2025-09-09 23:35:25 +0200

    development

--- analytics.py
+++ analytics.py
@@ -1,5 +1,5 @@
 from apscheduler.schedulers.background import BackgroundScheduler
-from datetime import datetime
+from datetime import datetime, timezone, timedelta
 import time
 import os
 
@@ -9,6 +9,7 @@ REDIS_HOST = os.getenv("REDIS_HOST", "localhost")
 REDIS_PORT = int(os.getenv("REDIS_PORT", "6379"))
 REDIS_DB = int(os.getenv("REDIS_DB", "0"))
 REQUEST_COUNTER_KEY = os.getenv("REQUEST_COUNTER_KEY", "github_requests_total")
+MINUTE_REQUEST_COUNTER_KEY: str = os.getenv("MINUTE_REQUEST_COUNTER_KEY", "minute_requests_counter")
 
 redis_client = redis.Redis(host=REDIS_HOST, port=REDIS_PORT, db=REDIS_DB, decode_responses=True)
 
@@ -25,6 +26,11 @@ def run_task3():
   try:
     count = redis_client.get(REQUEST_COUNTER_KEY) or 0
 
+    prev_minute = (datetime.now(timezone.utc).replace(second=0, microsecond=0) - timedelta(minutes=1))
+    minute_key = f"{MINUTE_REQUEST_COUNTER_KEY}:{prev_minute.strftime('%Y-%m-%d %H:%M')}"
+    # minute_key = f"{MINUTE_REQUEST_COUNTER_KEY}:{datetime.now(timezone.utc).strftime('%Y-%m-%d %H:%M')}"
+    minute_count = int(redis_client.get(minute_key) or 0)
+
     worker_keys = redis_client.keys("worker:*")
     workers = []
     for key in worker_keys:
@@ -32,6 +38,8 @@ def run_task3():
       workers.append(data)
 
     print(f">>>data from redis - {datetime.now().strftime('%H:%M:%S')} | total GitHub requests = {count}")
+    
+    print(f">>>requests per minute: {minute_count}")
 
     print(f"   active workers = {len(workers)}")
     for w in workers:
